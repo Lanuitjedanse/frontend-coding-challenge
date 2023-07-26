cec45ecd0e77b245478e264cb07f375b14b10d71
lucie bellenger <bellenger.lucie@gmail.com>
2023-07-26 20:00:18 +0200

    feat(VISABLE): create images service

---
+++ src/services/images.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const apiKey = process.env.VUE_APP_API_KEY;
+
+const images = {
+  async getImages(pageNumber = 1, roverName = "curiosity", cameras = "all") {
+    try {
+      const response = await fetch(
+        `https://api.nasa.gov/mars-photos/api/v1/rovers/${roverName}/photos?sol=1000&page=${pageNumber}&cameras=${cameras}&api_key=${apiKey}`
+      );
+      if (!response.ok) {
+        throw new Error("Network response was not ok.");
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Error fetching Curiosity manifest:", error);
+      throw error;
+    }
+  },
+};
+
+export default images;
