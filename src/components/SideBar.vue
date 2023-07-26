d1367362db7af8f3ded9de43b0a7067d2e6bd417
lucie bellenger <bellenger.lucie@gmail.com>
2023-07-26 20:00:04 +0200

    feat(VISABLE): create sidebar

---
+++ src/components/SideBar.vue
@@ -0,0 +1,42 @@
+<template>
+  <form class="sidebar__form">
+    <label for="type-select">Choose a photo type:</label>
+
+    <select v-model="type" name="types" id="type-select">
+      <option value="curiosity">Curiosity</option>
+      <option value="opportunity">Opportunity</option>
+      <option value="spirit">Spirit</option>
+    </select>
+
+    <button @click="onConfirm">Confirm</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "SideBar",
+  data() {
+    return {
+      type: "curiosity",
+    };
+  },
+  methods: {
+    onConfirm(event) {
+      console.log("this.type", this.type);
+      event.preventDefault();
+      this.$emit("onConfirm", this.type);
+    },
+  },
+};
+</script>
+
+<style>
+.sidebar__form {
+  padding: 1rem;
+  gap: 2rem;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  background-color: #0b3d91;
+}
+</style>
