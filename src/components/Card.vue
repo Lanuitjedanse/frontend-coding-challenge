6bdaedbb729124ceea602f16b791da671b32e4ae
lucie bellenger <bellenger.lucie@gmail.com>
2023-07-26 19:58:33 +0200

    feat(VISABLE): create card component and Grid

---
+++ src/components/Card.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="card__container">
+    <img :src="cardSrc" :alt="cardAlt" class="card__image" />
+    <p class="card__description">{{ cardDescription }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+    src: {
+      type: String,
+      default: "",
+    },
+    alt: {
+      type: String,
+      default: "",
+    },
+    description: {
+      type: String,
+      default: "",
+    },
+  },
+  computed: {
+    cardSrc() {
+      return this.src ? this.src : require("../assets/error.jpg");
+    },
+    cardAlt() {
+      return this.alt ? this.alt : "failed to load ressource";
+    },
+    cardDescription() {
+      return this.description
+        ? this.description
+        : "There was an error, please reload the page";
+    },
+  },
+};
+</script>
+
+<style>
+.card__container {
+  padding: 0.5rem;
+  border-radius: 0.2rem;
+  border: 0.1rem solid black;
+  max-width: fit-content;
+  transition: transform 500ms;
+  box-shadow: 0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1);
+}
+
+.card__container:hover {
+  transform: translateY(1rem);
+}
+
+.card__image {
+  width: 12rem;
+  height: 12rem;
+  object-fit: cover;
+}
+
+@media (min-width: 600px) {
+  .card__image {
+    width: 17rem;
+    height: 17rem;
+    object-fit: cover;
+  }
+}
+
+.card__description {
+  font-size: small;
+}
+</style>
---
+++ src/components/Grid.vue
@@ -0,0 +1,72 @@
+<template>
+  <div ref="imagesContainer" class="grid__container">
+    <Card
+      v-for="image in images"
+      :key="image.id"
+      :src="image.img_src"
+      :alt="image.camera.full_name"
+      :description="image.camera.full_name"
+    />
+  </div>
+</template>
+
+<script>
+import debounce from "lodash.debounce";
+import Card from "./Card";
+
+export default {
+  name: "Grid",
+  components: { Card },
+  props: {
+    images: {
+      type: Array,
+      default: () => [],
+    },
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  mounted() {
+    this.$refs.imagesContainer.addEventListener(
+      "scroll",
+      debounce(this.handleScroll, 1000)
+    );
+  },
+  beforeUnmount() {
+    this.$refs.imagesContainer.removeEventListener("scroll", this.handleScroll);
+  },
+  methods: {
+    handleScroll() {
+      const imagesContainer = this.$refs.imagesContainer;
+
+      if (imagesContainer) {
+        const isAtBottom =
+          imagesContainer.scrollTop + imagesContainer.clientHeight ===
+          imagesContainer.scrollHeight;
+
+        if (isAtBottom) {
+          this.$emit("getMoreImages");
+        }
+      }
+    },
+  },
+};
+</script>
+
+<style>
+.grid__container {
+  display: grid;
+  grid-row: 3 / 4;
+  gap: 30px;
+  grid-auto-flow: row;
+  width: 100%;
+  margin: 0 auto;
+  grid-template-columns: repeat(auto-fill, minmax(325px, 1fr));
+  justify-content: center;
+  justify-items: center;
+  padding-top: 2rem;
+  padding-bottom: 7rem;
+  overflow-x: hidden;
+}
+</style>
