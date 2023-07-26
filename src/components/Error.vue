a0fa731eeba05e7a034e7a4185e9d9cee2d49eb2
lucie bellenger <bellenger.lucie@gmail.com>
2023-07-26 19:59:49 +0200

    feat(VISABLE): create error component

---
+++ src/components/Error.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="error__container">
+    <img :src="icon" alt="error" class="error__image" />
+    <p class="error__text">Oops, there was an error! Please try again later</p>
+  </div>
+</template>
+
+<script>
+import circleX from "../assets/icons/circle-xmark-solid.svg";
+export default {
+  name: "Error",
+  data() {
+    return {
+      icon: circleX,
+    };
+  },
+};
+</script>
+
+<style>
+.error__container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.error__text {
+  font-size: small;
+}
+
+.error__image {
+  width: 5rem;
+}
+
+@media (min-width: 600px) {
+  .error__image {
+    width: 10rem;
+  }
+
+  .error__text {
+    font-size: large;
+  }
+}
+</style>
